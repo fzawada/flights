94bee31ce840faf0a66ee3fd601dd3c66418da16
Filip Zawada <fzawada@gmail.com>
2013-03-25 21:32:47 +0100

    Added missing files

---
+++ Flights/DepartureInTheFutureSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Flights
+{
+    public class DepartureInTheFutureSpecification
+    {
+        public bool IsSatisfiedBy(Flight flight)
+        {
+            if (flight == null)
+            {
+                throw new ArgumentNullException("flight");
+            }
+
+            //important to take into account local time vs UTC
+            var currentTime = DateTime.Now;
+
+            if (flight.Segments.First().DepartureDate < currentTime)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ FlightsTests/DepartureInTheFutureSpecificationTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Flights;
+using NUnit.Framework;
+
+namespace FlightsTests
+{
+    [TestFixture]
+    public class DepartureInTheFutureSpecificationTests
+    {
+        [Test]
+        public void Is_not_satisfied_if_departure_is_in_the_past()
+        {
+            //arrange
+            var sut = new DepartureInTheFutureSpecification();
+            var flight = new Flight
+                {
+                    Segments = new List<Segment>
+                            {
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddDays(-1),
+                                        ArrivalDate = DateTime.Now.AddHours(1)
+                                    },
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddHours(2),
+                                        ArrivalDate = DateTime.Now.AddHours(4)
+                                    }
+                            }
+                };
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.False(isSatisfied);
+        }
+
+
+        [Test]
+        public void Is_satisfied_if_departure_is_in_the_future()
+        {
+            //arrange
+            var sut = new DepartureInTheFutureSpecification();
+            var flight = new Flight
+            {
+                Segments = new List<Segment>
+                            {
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddHours(1),
+                                        ArrivalDate = DateTime.Now.AddHours(2)
+                                    },
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddHours(3),
+                                        ArrivalDate = DateTime.Now.AddHours(4)
+                                    }
+                            }
+            };
+
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.True(isSatisfied);
+        }
+
+    }
+
+}
