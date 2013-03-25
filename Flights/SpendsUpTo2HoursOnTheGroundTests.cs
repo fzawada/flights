888f23b1a430f522bef2648510c75655641e7175
Filip Zawada <fzawada@gmail.com>
2013-03-25 22:10:04 +0100

    Added 'Spends up to 2 hours on the ground' Specification

---
+++ Flights/SpendsUpTo2HoursOnTheGroundTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Flights;
+
+namespace FlightsTests
+{
+    public class SpendsUpTo2HoursOnTheGroundSpecification
+    {
+        public bool IsSatisfiedBy(Flight flight)
+        {
+            var totalTimeOnTheGround = TimeSpan.Zero;
+
+            if (flight.Segments.Count == 1)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < flight.Segments.Count - 1; i++)
+            {
+                var previousSegmentArrival = flight.Segments[i].ArrivalDate;
+                var nextSegmentDeparture = flight.Segments[i + 1].DepartureDate;
+                var flightChangeTime = nextSegmentDeparture - previousSegmentArrival;
+
+                totalTimeOnTheGround += flightChangeTime;
+            }
+
+            var isSatisfied = totalTimeOnTheGround <= TimeSpan.FromHours(2);
+            return isSatisfied;
+        }
+    }
+}
---
+++ FlightsTests/SpendsUpTo2HoursOnTheGroundSpecificationTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Flights;
+using NUnit.Framework;
+
+namespace FlightsTests
+{
+    [TestFixture]
+    public class SpendsUpTo2HoursOnTheGroundSpecificationTests
+    {
+        [Test]
+        public void Is_not_satisfied_if_spends_more_than_two_hours_on_the_ground()
+        {
+            //arrange
+            var sut = new SpendsUpTo2HoursOnTheGroundSpecification();
+            var flight = new Flight
+                {
+                    Segments = new List<Segment>
+                        {
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(1),
+                                    ArrivalDate = DateTime.Now.AddHours(2)
+                                },
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(3),
+                                    ArrivalDate = DateTime.Now.AddHours(4)
+                                },
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(6),
+                                    ArrivalDate = DateTime.Now.AddHours(7)
+                                }
+                        }
+                };
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.False(isSatisfied);
+        }
+
+        [Test]
+        public void Is_satisfied_if_spends_two_hours_on_the_ground()
+        {
+            //arrange
+            var sut = new SpendsUpTo2HoursOnTheGroundSpecification();
+            var flight = new Flight
+                {
+                    Segments = new List<Segment>
+                        {
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(1),
+                                    ArrivalDate = DateTime.Now.AddHours(2)
+                                },
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(3),
+                                    ArrivalDate = DateTime.Now.AddHours(4)
+                                },
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(5),
+                                    ArrivalDate = DateTime.Now.AddHours(6)
+                                }
+                        }
+                };
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.True(isSatisfied);
+        }
+
+        [Test]
+        public void Is_satisfied_if_spends_less_than_two_hours_on_the_ground()
+        {
+            //arrange
+            var sut = new SpendsUpTo2HoursOnTheGroundSpecification();
+            var flight = new Flight
+                {
+                    Segments = new List<Segment>
+                        {
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(1),
+                                    ArrivalDate = DateTime.Now.AddHours(2)
+                                },
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(3),
+                                    ArrivalDate = DateTime.Now.AddHours(4)
+                                },
+                            new Segment
+                                {
+                                    DepartureDate = DateTime.Now.AddHours(4).AddMinutes(30),
+                                    ArrivalDate = DateTime.Now.AddHours(6)
+                                }
+                        }
+                };
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.True(isSatisfied);
+        }
+    }
+}
