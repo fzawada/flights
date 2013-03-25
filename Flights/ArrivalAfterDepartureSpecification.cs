ed2aba520f3dbdc8b0a11db2864cc63d6e98e68e
Filip Zawada <fzawada@gmail.com>
2013-03-25 21:44:49 +0100

    Added 'Arrival after departure' Specification

---
+++ Flights/ArrivalAfterDepartureSpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Flights
+{
+    public class ArrivalAfterDepartureSpecification
+    {
+        public bool IsSatisfiedBy(Flight flight)
+        {
+            if (flight == null)
+            {
+                throw new ArgumentNullException("flight");
+            }
+
+            foreach (var segment in flight.Segments)
+            {
+                if (segment.ArrivalDate < segment.DepartureDate)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ FlightsTests/ArrivalAfterDepartureSpecificationTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Flights;
+using NUnit.Framework;
+
+namespace FlightsTests
+{
+    [TestFixture]
+    public class ArrivalAfterDepartureSpecificationTests
+    {
+        [Test]
+        public void Is_not_satisfied_if_any_arrival_is_before_its_corresponding_departure()
+        {
+            //arrange
+            var sut = new ArrivalAfterDepartureSpecification();
+            var flight = new Flight
+            {
+                Segments = new List<Segment>
+                            {
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddDays(-1),
+                                        ArrivalDate = DateTime.Now.AddHours(1)
+                                    },
+                                new Segment //invalid segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddHours(4),
+                                        ArrivalDate = DateTime.Now.AddHours(2)
+                                    }
+                            }
+            };
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.False(isSatisfied);
+        }
+
+
+        [Test]
+        public void Is_satisfied_if_all_arrivals_are_after_corresponding_departures()
+        {
+            //arrange
+            var sut = new ArrivalAfterDepartureSpecification();
+            var flight = new Flight
+            {
+                Segments = new List<Segment>
+                            {
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddHours(1),
+                                        ArrivalDate = DateTime.Now.AddHours(2)
+                                    },
+                                new Segment
+                                    {
+                                        DepartureDate = DateTime.Now.AddHours(3),
+                                        ArrivalDate = DateTime.Now.AddHours(4)
+                                    }
+                            }
+            };
+
+
+            //act
+            var isSatisfied = sut.IsSatisfiedBy(flight);
+
+            //assert
+            Assert.True(isSatisfied);
+        }
+    }
+}
+
